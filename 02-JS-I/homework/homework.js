bdb46544266ce167a602b88488910045911bd30e
juanmaarlenghi <Cucuia.96>
2022-05-18 18:37:38 -0300

    modif 18-05

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -260,7 +260,7 @@ function esVocal(letra){
 }
 
 // No modificar nada debajo de esta línea
-// --------------------------------
+// ---------------------------------
 
 module.exports = {
   nuevaString,
